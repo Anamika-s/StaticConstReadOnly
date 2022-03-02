793bdd717c1fe98a1c24d8ab5e41b2d6af8908e4
Anamika_S <anamika.sawhney22@gmail.com>
2022-03-02 09:21:28 +0000

    Add project files.

---
+++ StaticCOnstReadOnly/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticCOnstReadOnly
+{
+    class Employee
+    {
+        readonly int id;
+        string name;
+        static string manager;
+        public const string company="Dr.Lal Path Lab";
+        decimal salary;
+        static Employee()
+        {
+            manager = "Ajay";
+        }
+        public Employee()
+        {
+
+        }
+        public Employee(int id, string name, string manager,
+            string company, decimal salary)
+        {
+            this.id = id;
+            this.name = name;
+            manager = manager;
+            this.salary = salary;
+            company = company;
+
+        }
+        public void GetDetails()
+        {
+            //Console.WriteLine("Enter ID");
+            //    id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+                name = Console.ReadLine();
+            //Console.WriteLine("Enter Company Name");
+            
+            //company =  Console.ReadLine();
+            Console.WriteLine("Enter salary");
+            salary = Decimal.Parse(Console.ReadLine());
+           
+
+        }
+        public static void GetManager()
+        {
+            Console.WriteLine("Enter manager");
+            manager = Console.ReadLine();
+            Console.WriteLine("Manager is " + manager);
+        }
+    }
+}
---
+++ StaticCOnstReadOnly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StaticCOnstReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee employee = new Employee();
+            //Console.WriteLine(Employee.company);
+            //Employee.GetManager();
+            //String str = string.Empty;
+            //str += "Thid";
+            //str += "is";
+            //Console.WriteLine(str);
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append("This");
+            //sb.Append("is");
+            //sb.Insert(1, "that");
+            //Console.WriteLine(sb.ToString()) ;
+            CommonFunctions.NumericFunctions numericFunctions = new CommonFunctions.NumericFunctions();
+            Console.WriteLine(numericFunctions.Add(10,2,23,23,23,12));
+            Console.WriteLine(numericFunctions.IsEven(10));
+            CommonFunctions.StringFunctions stringFunctions = new CommonFunctions.StringFunctions();
+            Console.WriteLine(stringFunctions.CombineNames("Ajay", "Kumar"));
+            Console.WriteLine(stringFunctions.Table(3,2,9));
+            
+
+        }
+    }
+}
